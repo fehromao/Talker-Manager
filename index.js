cc9f425792892df17cdf44b2a4dc089dae141447
Fernanda Romao <Fernanda Romao>
2022-08-02 19:47:52 -0300

    finaliza requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const talkers = JSON.parse(await fs.readFile('talker.json', 'utf-8'));
+  if (talkers.length < 1) return res.status(200).json([]);
+
+  return res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
