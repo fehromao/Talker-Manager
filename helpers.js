8233feb55dc4dd3c8e4a751a8566faf8b2a3e3ea
Fernanda Romao <Fernanda Romao>
2022-08-03 11:14:52 -0300

    corrige identação na pasta helpers

--- helpers.js
+++ helpers.js
@@ -1,6 +1,9 @@
 const fs = require('fs/promises');
+const crypto = require('crypto');
 
 const readTalkersFile = async (path) =>  
   JSON.parse(await fs.readFile(path, 'utf-8'));
 
-module.exports = { readTalkersFile };
+const generateToken = () => crypto.randomBytes(8).toString('hex');  
+
+module.exports = { readTalkersFile, generateToken };
