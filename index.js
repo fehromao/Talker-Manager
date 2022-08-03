092a034cafffd8f60885577c7d93771ffb68412a
Fernanda Romao <Fernanda Romao>
2022-08-03 16:24:40 -0300

    refatora import funcao

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helpers = require('./helpers');
+const validationLogin = require('./midlleware/validationLogin');
 
 const app = express();
 app.use(bodyParser.json());
@@ -31,7 +32,7 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(200).json(findTalker);
 });
 
-app.post('/login', helpers.validationLogin, (req, res) => {
+app.post('/login', validationLogin, (req, res) => {
   const token = helpers.generateToken();
   res.status(200).json({ token });
 });
