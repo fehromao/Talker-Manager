b11cc40fcbc2bbe3dcd6ba3ef2c4441533c47066
Fernanda Romao <Fernanda Romao>
2022-08-03 16:24:54 -0300

    refatora pasta helpers

--- helpers.js
+++ helpers.js
@@ -6,27 +6,6 @@ const readTalkersFile = async (path) =>
 
 const generateToken = () => crypto.randomBytes(8).toString('hex');
 
-const validationLogin = (req, res, next) => {
-  const { email, password } = req.body;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!email) {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  }
-  if (!emailRegex.test(email)) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
-}
-
-  if (!password) { 
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' }); 
-}
-
-  if (password.length < 6) { 
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }  
-  next();
-};
-
 const writeTalker = () => {};
 
-module.exports = { readTalkersFile, generateToken, validationLogin, writeTalker };
+module.exports = { readTalkersFile, generateToken, writeTalker };
